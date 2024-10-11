3140e0b2891b8bfbf618a453e332d86137f220a0
pmalcius <pauliusmalcius@gmail.com>
2024-10-11 00:04:16 -0400

    Added json with all classes from ImageNet

--- models/model.py
+++ models/model.py
@@ -1,9 +1,16 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import json
+
+# Load ImageNet class names from a JSON file
+def load_class_names(file_path='imagenet_class_names.json'):
+    with open(file_path, 'r') as f:
+        class_names = json.load(f)
+    return class_names
 
 class ImageClassifier(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes=1000):
         super(ImageClassifier, self).__init__()
 
         # Load resNet18
@@ -15,8 +22,10 @@ class ImageClassifier(nn.Module):
         num_features = self.model.fc.in_features
         self.model.fc = nn.Linear(num_features, num_classes)
 
+    def forward(self, x):
+        return self.model(x)
 
-def load_model(model_path, num_classes, device):
+def load_model(model_path, num_classes=1000, device='cpu'):
     model = ImageClassifier(num_classes)
     
     model.load_state_dict(torch.load(model_path, map_location=device))
